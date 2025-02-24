c28b1da5faa69ad236f26fb8fd19d092a0ef01b6
Ovidiu Carlan <ovidiu.18.carlan@gmail.com>
2025-02-24 14:10:38 +0100

    First Commit

---
+++ src/test/java/com/example/doclink/DocLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doclink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
