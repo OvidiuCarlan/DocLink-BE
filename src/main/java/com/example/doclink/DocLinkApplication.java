e57e286169b30b6adaeba09449c474f78734653c
Ovidiu Carlan <ovidiu.18.carlan@gmail.com>
2025-02-25 19:20:23 +0100

    Test

--- src/main/java/com/example/doclink/DocLinkApplication.java
+++ src/main/java/com/example/doclink/DocLinkApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DocLinkApplication {
+    
 
     public static void main(String[] args) {
         SpringApplication.run(DocLinkApplication.class, args);
