c2fbcb3ae8ac626a5a656e4e7881f75c4fb86521
Ovidiu Carlan <ovidiu.18.carlan@gmail.com>
2025-05-12 15:08:33 +0200

    Docker
    Docker

--- api-gateway/src/main/java/com/docklink/apigateway/routes/Routes.java
+++ api-gateway/src/main/java/com/docklink/apigateway/routes/Routes.java
@@ -4,7 +4,6 @@ import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctio
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.function.RequestPredicate;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
@@ -14,20 +13,28 @@ public class Routes {
 
     @Bean
     public RouterFunction<ServerResponse> userServiceRoute(){
+        String userServiceUrl = System.getenv("USER_SERVICE_URL") != null ?
+                System.getenv("USER_SERVICE_URL") : "http://localhost:8080";
         return GatewayRouterFunctions.route("user_service")
-                .route(RequestPredicates.path("/users/**"), HandlerFunctions.http("http://localhost:8080"))
+                .route(RequestPredicates.path("/users/**"), HandlerFunctions.http(userServiceUrl))
                 .build();
     }
+
     @Bean
     public RouterFunction<ServerResponse> postServiceRoute(){
+        String postServiceUrl = System.getenv("POST_SERVICE_URL") != null ?
+                System.getenv("POST_SERVICE_URL") : "http://localhost:8081";
         return GatewayRouterFunctions.route("post_service")
-                .route(RequestPredicates.path("/posts/**"), HandlerFunctions.http("http://localhost:8081"))
+                .route(RequestPredicates.path("/posts/**"), HandlerFunctions.http(postServiceUrl))
                 .build();
     }
+
     @Bean
     public RouterFunction<ServerResponse> appointmentServiceRoute(){
+        String appointmentServiceUrl = System.getenv("APPOINTMENT_SERVICE_URL") != null ?
+                System.getenv("APPOINTMENT_SERVICE_URL") : "http://localhost:8082";
         return GatewayRouterFunctions.route("appointment_service")
-                .route(RequestPredicates.path("/appointments/**"), HandlerFunctions.http("http://localhost:8082"))
+                .route(RequestPredicates.path("/appointments/**"), HandlerFunctions.http(appointmentServiceUrl))
                 .build();
     }
 }
